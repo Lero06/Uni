96c2564d1bb88be210518b935b7404dce040cba4
Albert <57032316+Lero06@users.noreply.github.com>
2022-10-03 19:30:13 +0200

    Add files via upload

--- AVD/pract1-2.hs
+++ AVD/pract1-2.hs
@@ -13,4 +13,13 @@ miOrdered (x:y:ys) = x <= y && miOrdered (y:ys)
 ----------------------------------------------------------
 
 prop_Ordered :: Int -> [Int] -> Property
-prop_Ordered x (y:ys) = miInsert x (y:ys)
+prop_Ordered x ys = miOrdered ys == True ==> miOrdered (miInsert x (ys)) == True
+
+
+-- Ejercicio 9
+prop_Ordered2 :: Int -> [Int] -> Property
+prop_Ordered2 x ys = miOrdered ys == True ==> classify (null ys) "trivial" $ miOrdered (miInsert x (ys)) == True
+
+-- Ejercicio 10
+prop_Ordered3 :: Int -> [Int] -> Property
+prop_Ordered3 x ys = miOrdered ys == True ==> collect (length ys) $ miOrdered (miInsert x (ys)) == True
---
+++ AVD/pract1-3.hs
@@ -0,0 +1,4 @@
+import Test.QuickCheck
+
+prop_DoubleCycle :: [Int] -> Int -> Property
+prop_DoubleCycle xs n = not (null xs) && n >= 0 ==> take n (cycle xs) == take n (cycle (xs ++ xs))
